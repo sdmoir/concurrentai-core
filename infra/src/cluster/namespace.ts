5a58810818b82f0b6335dc6d392f650a8c47a507
Sean Moir <2264122+sdmoir@users.noreply.github.com>
2020-08-17 22:47:16 -0400

    Remove infra from repo and setup Docker Hub (#1)
    * Removed Pulumi infra and local docker configuration
    
    * Updated GitHub workflows to push to Docker Hub

--- infra/src/cluster/namespace.ts
+++
@@ -1,7 +0,0 @@
-import * as k8s from "@pulumi/kubernetes";
-
-import config from "../config";
-
-export const namespace = new k8s.core.v1.Namespace(
-  `rendezvous-${config.rendezvous.organizationId}`
-);
